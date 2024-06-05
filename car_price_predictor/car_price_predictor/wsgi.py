20f0b1e3a852cae505f1a0f5d757259e91190ee7
JinkoSiz <zpska1977@mail.ru>
2024-06-05 22:51:33 +0300

    Fix?

--- car_price_predictor/car_price_predictor/wsgi.py
+++ car_price_predictor/car_price_predictor/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'car_price_predictor.settings')
 
 application = get_wsgi_application()
+app = application
